ea09655c0baabbf6ee6fb0d9f8537051f63bf3af
Jakub Janik <n3hrox@ITJakubJanik.local>
2017-02-19 09:45:48 +0100

    created logger as singleton patter

---
+++ back/database_package/logger.py
@@ -0,0 +1,46 @@
+import getpass
+from datetime import datetime
+import time
+
+
+class LoggerSingleton:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        """Overload how the objects are created,
+        limit all of them to one instance"""
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        """add everything to one logger file,
+        can be changed to log into multiple ones"""
+        self.logger = "db_logger.log"
+
+    def log_saving(self, json):
+        with open(self._instance.logger, "a") as f:
+            f.write(
+                get_time() + "\nAdded to json collection:\n" + str(json) +
+                "\nby user: " + getpass.getuser() + "\n\n"
+            )
+
+    def log_reading(self):
+        with open(self._instance.logger, "a") as f:
+            f.write(
+                get_time() + "\nUser: " + getpass.getuser() +
+                " read the whole json collection\n\n"
+            )
+
+    def log_clear(self, json_collection):
+        with open(self._instance.logger, "a") as f:
+            f.write(
+                get_time() + "\nUser: " + getpass.getuser() +
+                " cleared the whole json collection \nLast known collection:" +
+                str(json_collection) + "\n\n"
+            )
+
+
+def get_time():
+    """simple function to determine timestamp of an action"""
+    return datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
--- back/database_package/repositoryDB.py
+++ back/database_package/repositoryDB.py
@@ -1,5 +1,6 @@
 import simplejson
 from pymongo import MongoClient
+from back.database_package.logger import LoggerSingleton
 
 
 class RepositoryDB:
@@ -8,22 +9,27 @@ class RepositoryDB:
         self.client = MongoClient()
         # retrieve proper collection with specified name
         self.json_collection = self.client[database_name][collection_name]
+        # create logger instance
+        self.logger = LoggerSingleton()
 
     def append(self, json):
         """
             Adds json list to database.
         """
         data = simplejson.loads(json)
+        self.logger.log_saving(json)
         self.json_collection.insert(data)
 
     def read_all(self):
         """
             Returns list of json.
         """
+        self.logger.log_reading()
         return simplejson.dumps([{i: x[i] for i in x if i != '_id'} for x in self.json_collection.find()])
 
     def clear(self):
         """
             Removes all documents and lists from json collection.
         """
+        self.logger.log_clear(list(self.json_collection.find()))
         self.json_collection.remove()
